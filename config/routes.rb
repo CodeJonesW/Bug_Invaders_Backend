acf0afada5a33926b979f2040b805c4ab373942c
Will Jones <willjones2010@gmail.com>
2019-08-07 14:21:16 -0400

    test remote

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # testchange For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
       resources :comments, only: [:index, :create, :update, :destroy]
